83e5a1b4e21b358767d9f2a3b6949ae80e7e899c
Ryan  Gunn <ryangunn@Ryans-MacBook-Air.local>
2015-01-08 16:58:53 -0600

    "First commit"

--- TicTaeToe/ViewController.m
+++ TicTaeToe/ViewController.m
@@ -9,6 +9,19 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UILabel *LabelOne;
+@property (strong, nonatomic) IBOutlet UILabel *LabelTwo;
+@property (strong, nonatomic) IBOutlet UILabel *LabelThree;
+@property (strong, nonatomic) IBOutlet UILabel *LabelFour;
+@property (strong, nonatomic) IBOutlet UILabel *LabelFive;
+@property (strong, nonatomic) IBOutlet UILabel *LabelSix;
+@property (strong, nonatomic) IBOutlet UILabel *LabelSeven;
+@property (strong, nonatomic) IBOutlet UILabel *LabelEight;
+@property (strong, nonatomic) IBOutlet UILabel *LabelNine;
+@property (strong, nonatomic) IBOutlet UILabel *whichPlayerLabel;
+@property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *labels;
+@property NSString* player;
+@property UIColor *playerColor;
 
 @end
 
@@ -16,12 +29,56 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.player = @"x";
+    self.playerColor = [UIColor blueColor];
+    self.whichPlayerLabel.text = @"PLAYER X";
+
+}
+- (IBAction)findLabelTapped:(UITapGestureRecognizer *)sender {
+    CGPoint point = [sender locationInView:self.view];
+    [self findLabelUsingPoint:point];
+
+
+
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+-(void) findLabelUsingPoint: (CGPoint)point{
+
+    NSLog(@"%f, %f",point.x, point.y);
+
+    for (UILabel* label in self.labels){
+        if (CGRectContainsPoint(label.frame, point)) {
+            label.text = self.player;
+            label.textColor = self.playerColor;
+            [self switchPlayer:label];
+        }
+    }
+
+
+
+
+
+
+}
+-(void)switchPlayer:(UILabel*)label{
+    if ([self.player isEqualToString:@"x"]) {
+        self.player = @"o";
+        self.playerColor = [UIColor redColor];
+        self.whichPlayerLabel.text = @"PLAYER O";
+
+
+
+    }
+    else
+    {
+        self.player= @"x";
+        self.playerColor = [UIColor blueColor];
+        self.whichPlayerLabel.text = @"PLAYER X";
+        
+        
+    }
 }
 
 @end
